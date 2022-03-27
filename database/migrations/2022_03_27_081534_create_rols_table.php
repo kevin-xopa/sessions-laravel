ff1ea9c967daae5722066a50f2f0711b4c2af566
Kevin Ochoa Xopa <kevinochoaxopa@gmail.com>
2022-03-27 04:06:09 -0600

    create migrations

--- database/migrations/2022_03_27_081534_create_rols_table.php
+++ database/migrations/2022_03_27_081534_create_rols_table.php
@@ -13,16 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rols', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
-            $table->foreignId('rol_id')->constrained('rols');
+            $table->string('description');
+            $table->string('slug');
             $table->timestamps();
         });
     }
@@ -34,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rols');
     }
 };
